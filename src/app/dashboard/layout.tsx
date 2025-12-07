e3308d5ff785c4c5d7efb19bcaf9fba20e7df518
Zakaria Boukhari <taxizak81@gmail.com>
2025-12-07 02:27:24 +0000

    Try fixing this error: `Console Error: You provided a `value` prop to a

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -46,6 +46,7 @@ export default function DashboardLayout({
                 <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
                 <Input
                   type="search"
+                  defaultValue=""
                   placeholder="Rechercher..."
                   className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
                 />
