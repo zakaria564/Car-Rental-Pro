a94f71710e26650636ceabf0a5522fb6c5ee2709
Zakaria Boukhari <taxizak81@gmail.com>
2026-01-25 20:42:07 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -47,6 +47,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.wizicar.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
