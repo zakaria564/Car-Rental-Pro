b7497a8dbd46344a031c970aded07db35482018a
Zakaria Boukhari <taxizak81@gmail.com>
2026-01-25 20:43:16 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -53,6 +53,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.leguideauto.ma',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
