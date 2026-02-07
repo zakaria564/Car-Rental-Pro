1b85c1256992a358384d99272f3cb66c1a915a7b
Zakaria Boukhari <taxizak81@gmail.com>
2026-02-07 22:50:24 +0000

    Try fixing this error: `Unhandled Runtime Error: ReferenceError: Dropdow

--- src/components/archives/archived-payments-table.tsx
+++ src/components/archives/archived-payments-table.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import * as React from "react";
@@ -23,6 +22,7 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
