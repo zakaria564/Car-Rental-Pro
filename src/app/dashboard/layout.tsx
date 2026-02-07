905de5bf0225a87149df3f4256e22d8da88ff083
Zakaria Boukhari <taxizak81@gmail.com>
2026-02-07 20:59:36 +0000

    tu dois le retirer in ne sérra rien (_for element input_)

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -10,7 +10,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-import { Search, Bell } from 'lucide-react';
+import { Search } from 'lucide-react';
 import { Input } from '@/components/ui/input';
 import { DashboardNav } from '@/components/dashboard-nav';
 import Link from 'next/link';
@@ -26,10 +26,6 @@ export default function DashboardLayout({
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <SidebarBrand />
-            <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
-              <Bell className="h-4 w-4" />
-              <span className="sr-only">Activer les notifications</span>
-            </Button>
           </div>
           <div className="flex-1">
             <DashboardNav />
