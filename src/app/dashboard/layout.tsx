ea4cefae25383c69c09fc5348b349822731450f3
Zakaria Boukhari <taxizak81@gmail.com>
2026-02-08 01:57:14 +0000

    Unhandled Runtime Error
    ChunkLoadError: Loading chunk app/dashboard/lay

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -35,17 +35,14 @@ export default function DashboardLayout({
       <div className="flex flex-col">
         <header className="flex h-14 items-center gap-4 border-b bg-card px-4 lg:h-[60px] lg:px-6">
           <div className="w-full flex-1">
-            <form>
-              <div className="relative">
-                <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-                <Input
-                  type="search"
-                  defaultValue=""
-                  placeholder="Rechercher..."
-                  className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
-                />
-              </div>
-            </form>
+            <div className="relative">
+              <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+              <Input
+                type="search"
+                placeholder="Rechercher..."
+                className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
+              />
+            </div>
           </div>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
