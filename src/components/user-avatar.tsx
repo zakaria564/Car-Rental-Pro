fdac1491d87a69619c89cb2ff218654b7156e325
Zakaria Boukhari <taxizak81@gmail.com>
2026-02-08 20:06:24 +0000

    modifier la forme étoile par la forme cercle (_for element html_)

--- src/components/user-avatar.tsx
+++ src/components/user-avatar.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import { useFirebase } from '@/firebase';
@@ -23,7 +22,7 @@ export function UserAvatar({ className }: { className?: string }) {
     const logoUrl = companySettings?.logoUrl;
 
     return (
-        <Avatar className={cn("bg-secondary h-full w-full [clip-path:polygon(50%_0%,_61%_35%,_98%_35%,_68%_57%,_79%_91%,_50%_70%,_21%_91%,_32%_57%,_2%_35%,_39%_35%)]", className)}>
+        <Avatar className={cn("rounded-full bg-secondary h-full w-full", className)}>
             {logoUrl ? (
                 <AvatarImage src={logoUrl} alt="Logo de l'agence" className="object-cover" />
             ) : (
